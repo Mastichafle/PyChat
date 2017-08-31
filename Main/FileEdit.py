896f87adfd420c4633785384f3e6efb8d655a06a
Kirsi <mushi88@users.noreply.github.com>
2017-08-31 14:46:42 +1000

    Update FileEdit.py

--- Main/FileEdit.py
+++ Main/FileEdit.py
@@ -1,25 +1,28 @@
 import os
 
-def ReadFile(File):
+def TestFile(File):
     try:
         File = open(File, "r")
         File.close()
+        return True
     except:
-        return None
-    
-    try:
-        File = open("Chat.txt", r)
-        File.close()
-        
-        os.remove("Chat.txt")
-        os.system("xcopy '"+File+"'")
-    except:
-        os.system("xcopy '"+File+"'")
-    
-    with open("Chat.txt", "r") as File:
-        lines = File.readlines()
-    
-    return lines
+        return False
+
+def ReadFile(File):
+    if TestFile(File):
+        try:
+            File = open("Chat.txt", r)
+            File.close()
+
+            os.remove("Chat.txt")
+            os.system("xcopy '"+File+"'")
+        except:
+            os.system("xcopy '"+File+"'")
+
+        with open("Chat.txt", "r") as File:
+            lines = File.readlines()
+
+        return lines
 
 def WriteFile(File, Data):
     while 1:
