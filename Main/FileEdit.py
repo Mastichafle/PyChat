22db0213c7ba305c399c4ffb05710534a00c06a6
Kirsi <mushi88@users.noreply.github.com>
2017-08-31 14:42:33 +1000

    Update FileEdit.py

--- Main/FileEdit.py
+++ Main/FileEdit.py
@@ -1,1 +1,31 @@
-# look up xcopy
+import os
+
+def ReadFile(File):
+    try:
+        File = open(File, "r")
+        File.close()
+    except:
+        return None
+    
+    try:
+        File = open("Chat.txt", r)
+        File.close()
+        
+        os.remove("Chat.txt")
+        os.system("xcopy '"+File+"'")
+    except:
+        os.system("xcopy '"+File+"'")
+    
+    with open("Chat.txt", "r") as File:
+        lines = File.readlines()
+    
+    return lines
+
+def WriteFile(File, Data):
+    while 1:
+        try:
+            with open(File, "w") as File:
+                File.write(Data)
+            break
+        except:
+            continue
