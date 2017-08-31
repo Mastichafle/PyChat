f06fe4befb9c02e73ad638478659dab3b4b1a5d1
Kirsi <mushi88@users.noreply.github.com>
2017-08-31 15:07:38 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import os
 
-from Main import EditFile
+from Main import EditFile as EF
+
+chatFile = "P:/Composite/All Students/Chat.txt"
+nameFile = "U:/Name.txt"
+name = ""
+
+def Setup():
+    global name
+    
+    if not EF.TestFile(chatFile):
+        File = open(chatFile, "w")
+        File.close()
+   
+    #if not EF.TestFile(nameFile):
+        #while Name == "":
+            
+    
+if __name__ == "__main__":
+    Setup()
